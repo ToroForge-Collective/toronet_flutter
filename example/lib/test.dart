71119770f74553b865c8d43bc24031b96345e59f
milimeter <razaqolad300@gmail.com>
2025-08-17 23:59:58 +0100

    Enhance wallet and blockchain functionalities in test file
    - Updated the test file to create a wallet with specific credentials and retrieve its address.
    - Added functionality to fetch wallet key, blockchain status, blocks data, and wallet balance.
    - Implemented wallet import from private key and KYC verification status checks.
    - Included retrieval of supported assets exchange rates for improved testing capabilities.

--- example/lib/test.dart
+++ example/lib/test.dart
@@ -1,16 +1,48 @@
+// ignore_for_file: avoid_print
+
 import 'package:toronet/toronet.dart';
 
 main() async {
-  final ToronetSDK sdk = ToronetSDK(baseUrl: 'https://www.toronet.org');
-  // Wallet wallet = await sdk.walletService.createWallet(
-  //   username: 'testuser664',
-  //   password: 'testpassword',
-  // );
+  final ToronetSDK sdk = ToronetSDK();
+  Wallet wallet = await sdk.walletService.createWallet(
+    username: 'itadori_yuji',
+    password: 'sukuna',
+  );
 
-  // print("Wallet created: ${wallet.address}");
+  print("Wallet created: ${wallet.address}");
 
-  // print("generated tns : ${wallet.tnsName}");testuser664
+  final key = await sdk.walletService.getWalletKey(
+    address: "0x03c8ef05663bd8e4ad8074d650e4ccd33310cd95",
+    password: "testpassword",
+  );
+  print("wallet key: $key");
 
-  final key = await sdk.walletService.getWalletKey(address: "0x03c8ef05663bd8e4ad8074d650e4ccd33310cd95", password: "testpassword");
-  print("wallet key: ${key}");
+  var blockchainStatus = await sdk.blockchainService.getBlockchainStatus();
+  print("blockchain status: $blockchainStatus");
+
+  var blocksData = await sdk.blockchainService.getBlocksData(10);
+  print("blocks data: $blocksData");
+
+  var latestBlockData = await sdk.blockchainService.getLatestBlockData();
+  print("latest block data: $latestBlockData");
+
+  var walletDetails = await sdk.walletService.importWalletFromPrivateKey(
+    privateKey: "0x03c8ef05663bd8e4ad8074d650e4ccd33310cd95",
+    password: "testpassword",
+  );
+  print("wallet details: $walletDetails");
+
+  var walletBalance = await sdk.balanceService.getBalance(
+    address: "0x03c8ef05663bd8e4ad8074d650e4ccd33310cd95",
+  );
+  print("wallet balance: $walletBalance");
+
+  var kycStatus = await sdk.kycService.isAddressKYCVerified(
+    address: "0x03c8ef05663bd8e4ad8074d650e4ccd33310cd95",
+  );
+  print("kyc status: $kycStatus");
+
+  var exchangeRates =
+      await sdk.exchangeService.getSupportedAssetsExchangeRates();
+  print("exchange rates: $exchangeRates");
 }
