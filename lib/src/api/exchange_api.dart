5354b2cbf4fab80f7df2c32a1c58c0e04e6558d1
milimeter <razaqolad300@gmail.com>
2025-08-18 00:10:10 +0100

    Refactor exchange rate fetching logic in exchange_api.dart
    - Updated the fetchSupportedAssetsExchangeRates function to use a new API endpoint and request structure.
    - Changed the request method to GET with JSON content type for improved compatibility.
    - Minor adjustment in exchange_service_impl.dart to maintain consistency in method calls.

--- lib/src/api/exchange_api.dart
+++ lib/src/api/exchange_api.dart
@@ -4,6 +4,18 @@ Future<Map<String, dynamic>> fetchSupportedAssetsExchangeRates(
   Dio dio,
   String baseUrl,
 ) async {
-  final response = await dio.get('$baseUrl/exchange/rates');
-  return response.data;
+  final String url = '$baseUrl/api/query';
+  final data = {'op': 'getexchangerates', 'params': [
+     
+    ],
+  };
+  final response = await dio.request(
+    url,
+    data: data,
+    options: Options(
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    ),
+  );
+  return response.data as Map<String, dynamic>;
 }
