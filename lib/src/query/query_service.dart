595626457c90916b8d5aae22e8de718f4b8b8c5c
milimeter <razaqolad300@gmail.com>
2025-11-18 17:18:07 +0100

    Add additional methods to QueryService for enhanced querying capabilities
    - Expanded the QueryService abstract class by adding multiple methods for retrieving exchange rates, address roles, balances, transactions, and transaction history across various currencies.
    - This enhancement improves the flexibility and functionality of the querying operations within the application.

--- lib/src/query/query_service.dart
+++ lib/src/query/query_service.dart
@@ -18,4 +18,57 @@ abstract class QueryService {
     required String addr,
     required int count,
   });
+  Future<Map<String, dynamic>> getExchangeRates();
+  Future<Map<String, dynamic>> getAddrRole({required String addr});
+  Future<Map<String, dynamic>> getAddrBalance({required String addr});
+  Future<Map<String, dynamic>> getBlock({required String blockId});
+  Future<Map<String, dynamic>> getTransaction({required String hash});
+  Future<Map<String, dynamic>> getReceipt({required String hash});
+  Future<List<Map<String, dynamic>>> getTransactionsNaira({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsNaira({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsEuro({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsEuro({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsPound({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsPound({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsEgp({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsEgp({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsKsh({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsKsh({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsZar({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsZar({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsEth({required int count});
+  Future<List<Map<String, dynamic>>> getAddrTransactionsEth({
+    required String addr,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getTransactionsRange({
+    required String startDate,
+    required String endDate,
+    required int count,
+  });
+  Future<List<Map<String, dynamic>>> getAddrTransactionsRange({
+    required String addr,
+    required String startDate,
+    required String endDate,
+    String? token,
+    required int count,
+  });
 }
